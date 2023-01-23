db42d762520271bc756a192c299fb712ae99ff5e
echus <varvara@echus.co>
2023-01-23 11:29:54 +1100

    Initial Framework laptop configuration

---
+++ configuration.nix
@@ -0,0 +1,133 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports =
+    [
+      ./hardware-configuration.nix
+      # To use home-manager as a module: <home-manager/nixos>
+    ];
+
+  # Hardware config
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-label/swap"; }
+    ];
+
+  # Boot loader - systemd-boot
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi = {
+      canTouchEfiVariables = true;
+    };
+  };
+
+  # 12th gen bug fix - see nixos.wiki/Intel_Graphics
+  boot.kernelParams = [ "i915.force_probe=46a6" ];
+
+  networking = {
+    hostName = "btw";
+    wireless.iwd.enable = true;
+  };
+
+  time.timeZone = "Australia/Canberra";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; # use xkbOptions in tty
+  };
+
+  # Configure console keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbOptions = "caps:swapescape";
+  };
+
+  # Enable zsh and set as default for all users
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh ];
+
+  # Enable neovim as editor system-wide
+  environment.variables.EDITOR = "nvim";
+
+  # Enable polkit for sway
+  security.polkit.enable = true;
+
+  # Enable XDG portal for Wayland
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-wlr xdg-desktop-portal-gtk ];
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+
+    # Extra packages required for video acceleration
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+  # User account definition
+  users.users.varvara = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "video"
+      "audio"
+    ];
+    initialPassword = "pw123";
+  };
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+  ];
+
+  # System profile packages
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    zsh
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+}
---
+++ home.nix
@@ -0,0 +1,332 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.username = "varvara";
+  home.homeDirectory = "/home/varvara";
+
+  home.packages = with pkgs; [
+    oh-my-zsh
+    silver-searcher
+    fzf
+    tmux
+    foot
+    i3status-rust
+    bemenu
+    xwayland
+    gammastep
+    chromium
+    iwgtk
+    blueman
+    pamixer
+    neofetch
+    git
+    tig
+    python3
+    cmake
+    gcc
+    gnumake
+    nodejs
+    autotiling
+    gammastep
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    NIXOS_OZONE_WL = "1";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "sway";
+    XDG_CURRENT_DESKTOP = "sway";
+    SDL_VIDEODRIVER = "wayland";
+    GDK_BACKEND = "wayland";
+    QT_QPA_PLATFORM = "wayland";
+    BMENU_BACKEND = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
+  };
+
+  home.pointerCursor = {
+    name = "Adwaita";
+    package = pkgs.gnome.adwaita-icon-theme;
+    size = 24;
+    x11 = {
+      enable = true;
+      defaultCursor = "Adwaita";
+    };
+  };
+
+  wayland.windowManager.sway = {
+    enable = true;
+
+    xwayland = true;
+    systemdIntegration = true;
+
+    wrapperFeatures = {
+      base = true;
+      gtk = true;
+    };
+
+    config = {
+      terminal = "foot";
+      menu = "bemenu-run";
+      modifier = "Mod4";
+
+      input = {
+        "type:touchpad" = {
+          tap = "enabled";
+          dwt = "enabled";
+          scroll_method = "two_finger";
+          middle_emulation = "enabled";
+          natural_scroll = "enabled";
+        };
+
+        "type:pointer" = {
+          natural_scroll = "enabled";
+        };
+
+        "type:keyboard" = {
+          repeat_delay = "300";
+          repeat_rate = "30";
+          xkb_options = "caps:swapescape";
+        };
+      };
+
+      bars = [
+        {
+          position = "top";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
+        }
+      ];
+
+      fonts = {
+        names = [ "Fira Code" ];
+        size = 24.0;
+      };
+
+      gaps = {
+        inner = 5;
+        smartGaps = true;
+        smartBorders = "on";
+      };
+
+      window = {
+        border = 0;
+        titlebar = false;
+      };
+
+      startup = [
+        # https://github.com/NixOS/nixpkgs/issues/119445
+        {command = "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK";}
+      ];
+
+      output = {
+        "*".bg = "~/wallpapers/current.jpg fill";
+        "*".scale = "1.5";
+      };
+
+      # Display device configuration
+      # output = {
+      #   eDP-1 = {
+      #     # Set HIDP scale (pixel integer scaling)
+      #     scale = "2";
+      #   };
+      # };
+    };
+  };
+
+  programs.i3status-rust = {
+    enable = true;
+
+    bars.top = {
+      blocks = [
+                {
+          block = "cpu";
+          icons_format = " ";
+          format = "CPU: {utilization}";
+        }
+        {
+          block = "memory";
+          format_mem = "MEM: {mem_used_percents}";
+          display_type = "memory";
+          icons = false;
+          clickable = false;
+          warning_mem = 80;
+          critical_mem = 95;
+        }
+        # {
+        #   block = "sound";
+        #   icons_format = " ";
+        #   format = "VOL: {volume}";
+        #   on_click = "pamixer -t";
+        # }
+        {
+          block = "battery";
+          icons_format = " ";
+          format = "BAT: {percentage} {time}";
+          full_format = "BAT: {percentage} ";
+          full_threshold = 96;
+        }
+        {
+          block = "time";
+          icons_format = " ";
+          format = "%A, %b %_d";
+        }
+        {
+          block = "time";
+          icons_format = " ";
+          format = "%H:%M";
+        }
+      ];
+
+      # settings = {
+      #   theme = {
+      #     name = "native";
+      #     overrides = {
+      #       idle_bg = theme.background;
+      #       idle_fg = theme.foreground;
+      #     };
+      #   };
+      # };
+    };
+  };
+
+  programs.foot = {
+    enable = true;
+
+    settings = {
+      main = {
+        term = "xterm-256color";
+        font = "Fira Code:size=8";
+        dpi-aware = "yes";
+      };
+
+      mouse = {
+        hide-when-typing = "yes";
+      };
+
+      colors = {
+        foreground = "a0abae";
+        background = "202c3d";
+        regular0   = "202c3d";
+        regular1   = "fe6f70";
+        regular2   = "4eb075";
+        regular3   = "ba9a0a";
+        regular4   = "60a1e6";
+        regular5   = "d285ad";
+        regular6   = "3dae9f";
+        regular7   = "949cbf";
+        bright0    = "00B4D8";
+        bright1    = "ec7f4f";
+        bright2    = "5baf4f";
+        bright3    = "be981f";
+        bright4    = "8599ef";
+        bright5    = "cc82d7";
+        bright6    = "2aacbf";
+        bright7    = "a0abae";
+      };
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+
+    extraConfig = ''
+      " auto-install vim-plug
+      if empty(glob('~/.config/nvim/autoload/plug.vim'))
+        silent !curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        autocmd VimEnter * PlugInstall
+      endif
+
+      " vim-plug plugins
+      call plug#begin('~/.config/nvim/plugged')
+      Plug 'jeffkreeftmeijer/neovim-sensible'
+      Plug 'lervag/vimtex'
+      Plug 'hashivim/vim-terraform'
+      Plug 'mustache/vim-mustache-handlebars'
+      Plug 'arcticicestudio/nord-vim'
+      Plug 'tpope/vim-fugitive'
+      Plug 'tmhedberg/SimpylFold'
+      Plug 'Vimjas/vim-python-pep8-indent'
+      Plug 'ycm-core/YouCompleteMe'
+      Plug 'vim-syntastic/syntastic'
+      Plug 'nvie/vim-flake8'
+      Plug 'scrooloose/nerdtree'
+      Plug 'ctrlpvim/ctrlp.vim'
+      Plug 'joukevandermaas/vim-ember-hbs'
+      Plug 'LnL7/vim-nix'
+      call plug#end()
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+
+    userName = "echus";
+    userEmail = "varvara@echus.co";
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    autocd = true;
+    dotDir = ".config/zsh";
+    enableAutosuggestions = true;
+    enableCompletion = true;
+
+    shellAliases = {
+      # vim = "nvim";
+    };
+
+    initExtra = ''
+      # Rebind autocomplete/search key
+      bindkey "''${key[Up]}" up-line-or-search
+
+      # Start sway
+      if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
+        exec sway
+      fi
+    '';
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+
+      plugins = [
+        "git"
+        "node"
+        "npm"
+        "extract"
+        "z"
+        "terraform"
+      ];
+
+      theme = "blinks";
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+
+    enableZshIntegration = true;
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
